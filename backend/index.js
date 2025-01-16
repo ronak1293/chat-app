150bc288f8473b2826a146d05dbbbbfe6d4d9027
Ronak Rewar <128736765+ronak1293@users.noreply.github.com>
2025-01-16 22:03:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -44,7 +44,7 @@ const server=app.listen(PORT,console.log('server started'));
 const io=require("socket.io")(server,{
   pingTimeout:60000,
   cors:{
-    origin:"http://localhost:5173",
+    origin:"https://chat-app-2-dpok.onrender.com",
   }
 })
 
